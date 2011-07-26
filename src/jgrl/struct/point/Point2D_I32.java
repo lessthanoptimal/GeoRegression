0edefa499035b5aef430e461e4d76ddf9265136a
Peter Abeles <peter.abeles@gmail.com>
2011-07-26 09:26:01 -0400

    - Added copy and distance to Point2D_I32

--- src/jgrl/struct/point/Point2D_I32.java
+++ src/jgrl/struct/point/Point2D_I32.java
@@ -1,20 +1,17 @@
 /*
- * Copyright (c) 2011, Peter Abeles. All Rights Reserved.
+ * Copyright 2011 Peter Abeles
  *
- * This file is part of Java Geometric Regression Library (JGRL).
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * JGRL is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * JGRL is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with JGRL.  If not, see <http://www.gnu.org/licenses/>.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package jgrl.struct.point;
@@ -29,6 +26,11 @@ public class Point2D_I32 {
 	public Point2D_I32( int x, int y ) {
 		this.x = x;
 		this.y = y;
+	}
+
+	public Point2D_I32( Point2D_I32 orig ) {
+		this.x = orig.x;
+		this.y = orig.y;
 	}
 
 	public Point2D_I32() {
@@ -53,6 +55,10 @@ public class Point2D_I32 {
 
 	public final int getY() {
 		return y;
+	}
+
+	public Point2D_I32 copy() {
+		return new Point2D_I32( this );
 	}
 
 	@Override
