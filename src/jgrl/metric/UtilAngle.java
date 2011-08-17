52589b4825114bdcbad24f0af8fa353a210e8d50
Peter Abeles <peter.abeles@gmail.com>
2011-08-17 17:38:42 -0400

    - Fixed a bug in SteerableKernel_F32 - Added more unit tests - Added orientation algorithms   * more type specific histogram   * average based

--- src/jgrl/metric/UtilAngle.java
+++ src/jgrl/metric/UtilAngle.java
@@ -1,20 +1,17 @@
 /*
- * Copyright (c) 2011, Peter Abeles. All Rights Reserved.
+ * Copyright 2011 Peter Abeles
  *
- * This file is part of Java Geometric Regression Library (JGRL).
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * JGRL is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * JGRL is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with JGRL.  If not, see <http://www.gnu.org/licenses/>.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package jgrl.metric;
@@ -81,5 +78,19 @@ public class UtilAngle {
 			return -PI2 - diff;
 
 		return diff;
+	}
+
+	/**
+	 * <p>
+	 * Returns the number of radians two angles are apart.  This is equivalent to
+	 * Math.abs(UtilAngle.minus(angA,angB)).
+	 * </p>
+	 *
+	 * @param angA first angle.  Must be between -pi and pi.
+	 * @param angB second angle. Must be between -pi and pi.
+	 * @return an angle between 0 and pi
+	 */
+	public static double dist( double angA, double angB ) {
+		return Math.abs(minus(angA,angB));
 	}
 }
--- test/jgrl/metric/TestUtilAngle.java
+++ test/jgrl/metric/TestUtilAngle.java
@@ -1,20 +1,17 @@
 /*
- * Copyright (c) 2011, Peter Abeles. All Rights Reserved.
+ * Copyright 2011 Peter Abeles
  *
- * This file is part of Java Geometric Regression Library (JGRL).
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * JGRL is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * JGRL is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with JGRL.  If not, see <http://www.gnu.org/licenses/>.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package jgrl.metric;
@@ -64,5 +61,10 @@ public class TestUtilAngle {
 		assertEquals( -PI, minus( -0.9 * PI, 0.1 * PI ), 1e-5 );
 		assertEquals( 0.2 * PI, minus( 0.9 * PI, -0.9 * PI ), 1e-5 );
 		assertEquals( -0.2 * PI, minus( -0.9 * PI, 0.9 * PI ), 1e-5 );
+	}
+
+	@Test
+	public void testDist() {
+		fail("implement");
 	}
 }
