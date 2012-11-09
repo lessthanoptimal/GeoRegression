f7cf65b00a6f7ea857f1f3699d1c7dc3e3592df1
Peter Abeles <peter.abeles@gmail.com>
2012-11-09 10:07:09 -0500

    Fixed minor type-o in copyright notice

--- examples/src/georegression/examples/ExampleRotationParameterizations.java
+++ examples/src/georegression/examples/ExampleRotationParameterizations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2011-2012, Peter Abeles. All Rights Reserved.
  *
  * This file is part of Geometric Regression Library (GeoRegression).
  *
@@ -8,7 +8,7 @@
  * published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
  *
- * JGRL is distributed in the hope that it will be useful,
+ * GeoRegression is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
