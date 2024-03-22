81e800b327b868f1961cc25ab98cd454e57a36c7
Peter Abeles <peter.abeles@gmail.com>
2024-03-22 08:51:53 -0700

    - LineSegment3D   * Added pointOnLine()

--- main/src/georegression/struct/line/LineSegment3D_F64.java
+++ main/src/georegression/struct/line/LineSegment3D_F64.java
@@ -21,6 +21,7 @@ package georegression.struct.line;
 import georegression.struct.point.Point3D_F64;
 import lombok.Getter;
 import lombok.Setter;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.Serializable;
 
@@ -75,12 +76,49 @@ public class LineSegment3D_F64 implements Serializable {
 		b.zero();
 	}
 
+	/**
+	 * Computes a point on the line based on the fraction distance between 'a' and 'b'
+	 */
+	public Point3D_F64 pointOnLine( double fraction, @Nullable Point3D_F64 p ) {
+		if (p == null)
+			p = new Point3D_F64();
+		p.x = axisOnLineX(fraction);
+		p.y = axisOnLineY(fraction);
+		p.z = axisOnLineZ(fraction);
+		return p;
+	}
+
+	/**
+	 * Value of x-axis for a point on the line at this fractional location between 'a' and 'b'
+	 */
+	public double axisOnLineX( double fraction ) {
+		return slopeX()*fraction + a.x;
+	}
+
+	/**
+	 * Value of y-axis for a point on the line at this fractional location between 'a' and 'b'
+	 */
+	public double axisOnLineY( double fraction ) {
+		return slopeY()*fraction + a.y;
+	}
+
+	/**
+	 * Value of z-axis for a point on the line at this fractional location between 'a' and 'b'
+	 */
+	public double axisOnLineZ( double fraction ) {
+		return slopeZ()*fraction + a.z;
+	}
+
 	public double slopeX() {
 		return b.x - a.x;
 	}
 
 	public double slopeY() {
 		return b.y - a.y;
+	}
+
+	public double slopeZ() {
+		return b.z - a.z;
 	}
 
 	public double getLength() {
--- main/test/georegression/struct/line/TestLineSegment3D_F64.java
+++ main/test/georegression/struct/line/TestLineSegment3D_F64.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2020, Peter Abeles. All Rights Reserved.
+ * Copyright (C) 2022, Peter Abeles. All Rights Reserved.
  *
  * This file is part of Geometric Regression Library (GeoRegression).
  *
@@ -24,14 +24,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-/**
- * @author Peter Abeles
- */
 public class TestLineSegment3D_F64 {
-	@Test
-	void equals() {
-		LineSegment3D_F64 a = new LineSegment3D_F64(1,2,3,4,5,6);
-		LineSegment3D_F64 b = new LineSegment3D_F64(1,2,3,4,5,6);
+	@Test void pointOnLine() {
+		var segment = new LineSegment3D_F64(1, 2, 3, 5, 7, 9);
+
+		assertEquals(0, segment.pointOnLine(0.0, null).distance(1, 2, 3));
+		assertEquals(0, segment.pointOnLine(1.0, null).distance(5, 7, 9));
+	}
+
+	@Test void equals() {
+		LineSegment3D_F64 a = new LineSegment3D_F64(1, 2, 3, 4, 5, 6);
+		LineSegment3D_F64 b = new LineSegment3D_F64(1, 2, 3, 4, 5, 6);
 
 		assertEquals(a, b);
 		b.a.x += UtilEjml.TEST_F64;
