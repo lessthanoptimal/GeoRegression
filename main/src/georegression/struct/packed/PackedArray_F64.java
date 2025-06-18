668082b06814ebcd1ff8ef68c85e94babba41477
Peter Abeles <peter.abeles@gmail.com>
2025-06-18 07:41:41 -0700

    PackedArrays - Copied over packed arrays for points from BoofCV GeoStandardJUnit - This project was missing standard unit tests

---
+++ main/src/georegression/struct/packed/PackedArray_F64.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2025, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of Geometric Regression Library (GeoRegression).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package georegression.struct.packed;
+
+import lombok.Getter;
+import org.ddogleg.struct.DogArray_F64;
+
+public abstract class PackedArray_F64<T> implements PackedArray<T> {
+	/** Degrees of freedom */
+	protected final int DOF;
+
+	/** Stores tuple in a single continuous array */
+	@Getter protected final DogArray_F64 array;
+
+	public PackedArray_F64( int DOF, int length ) {
+		this.DOF = DOF;
+		this.array = new DogArray_F64(length);
+	}
+
+	@Override public void removeSwap( int index ) {
+		int idx0 = index*DOF;
+		int idx1 = idx0 + DOF;
+		int tail = array.size - DOF;
+		for (int j = idx0; j < idx1; j++) {
+			array.data[j] = array.data[tail++];
+		}
+		array.size -= DOF;
+	}
+
+	@Override public PackedArray_F64<T> reset() {
+		array.reset();
+		return this;
+	}
+
+	@Override public PackedArray_F64<T> reserve( int numTuples ) {
+		array.reserve(numTuples*DOF);
+		return this;
+	}
+
+	@Override public int size() {
+		return array.size/DOF;
+	}
+
+	@Override public boolean isEquals( PackedArray<T> b ) {return array.isEquals(((PackedArray_F64<T>)b).array);}
+
+	/** True if the two arrays are equal to within the specified tolerance */
+	public boolean isEquals( PackedArray<T> b, double tol ) {return array.isEquals(((PackedArray_F64<T>)b).array, tol);}
+}
---
+++ main/test/georegression/struct/packed/GenericPackedArrayChecks.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (C) 2025, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of Geometric Regression Library (GeoRegression).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package georegression.struct.packed;
+
+import georegression.GeoStandardJUnit;
+import org.ddogleg.struct.DogArray_I32;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Common tests for implementations of {@link PackedArray}.
+ */
+public abstract class GenericPackedArrayChecks<T> extends GeoStandardJUnit {
+	/** Creates a new instance of the algorithm being tested */
+	protected abstract PackedArray<T> createAlg();
+
+	/** Creates a random point */
+	protected abstract T createRandomPoint();
+
+	/** Checks to see if the two elements are identical */
+	protected abstract void checkEquals( T a, T b );
+
+	protected abstract void checkNotEquals( T a, T b );
+
+	@Test public void reset() {
+		PackedArray<T> alg = createAlg();
+
+		assertEquals(0, alg.size());
+		for (int i = 0; i < 3; i++) {
+			alg.append(createRandomPoint());
+		}
+		assertEquals(3, alg.size());
+
+		alg.reset();
+		assertEquals(0, alg.size());
+	}
+
+	@Test public void reserve() {
+		PackedArray<T> alg = createAlg();
+		alg.reserve(12);
+		assertEquals(0, alg.size());
+
+		for (int i = 0; i < 3; i++) {
+			alg.append(createRandomPoint());
+		}
+		assertEquals(3, alg.size());
+	}
+
+	@Test public void removeSwap() {
+		PackedArray<T> alg = createAlg();
+		T p0 = createRandomPoint();
+		T p1 = createRandomPoint();
+		T p2 = createRandomPoint();
+
+		alg.append(p0);
+		alg.append(p1);
+		alg.append(p2);
+
+		alg.removeSwap(1);
+		assertEquals(2, alg.size());
+		checkEquals(p0, alg.getTemp(0));
+		checkEquals(p2, alg.getTemp(1));
+
+		alg.removeSwap(1);
+		assertEquals(1, alg.size());
+		checkEquals(p0, alg.getTemp(0));
+
+		alg.removeSwap(0);
+		assertEquals(0, alg.size());
+	}
+
+	@Test public void addCopy_getCopy() {
+		PackedArray<T> alg = createAlg();
+
+		// Create two different points
+		T a = createRandomPoint();
+		T b = createRandomPoint();
+
+		// Add the first point
+		assertEquals(0, alg.size());
+		alg.append(a);
+		assertEquals(1, alg.size());
+
+		// Copy the first point into the second
+		checkNotEquals(a, b);
+		alg.getCopy(0, b);
+		checkEquals(a, b);
+	}
+
+	@Test public void addCopy_getCopy_multiple() {
+		PackedArray<T> alg = createAlg();
+		List<T> expected = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			T a = createRandomPoint();
+			expected.add(a);
+			alg.append(a);
+		}
+
+		assertEquals(5, alg.size());
+
+		T b = createRandomPoint();
+		for (int i = 0; i < 5; i++) {
+			alg.getCopy(i, b);
+			checkEquals(expected.get(i), b);
+		}
+	}
+
+	@Test public void getTemp() {
+		PackedArray<T> alg = createAlg();
+
+		T a = createRandomPoint();
+
+		alg.append(a);
+		// The returned item should not be the same instance
+		assertNotSame(a, alg.getTemp(0));
+
+		// They should have an identical value
+		checkEquals(a, alg.getTemp(0));
+	}
+
+	@Test public void copy() {
+		T a = createRandomPoint();
+		T b = createRandomPoint();
+
+		PackedArray<T> alg = createAlg();
+		alg.copy(a, b);
+		checkEquals(a, b);
+	}
+
+	@Test public void forIdx() {
+		PackedArray<T> array = createAlg();
+		for (int i = 0; i < 26; i++) {
+			array.append(createRandomPoint());
+		}
+
+		List<T> found = new ArrayList<>();
+		DogArray_I32 indexes = new DogArray_I32();
+
+		array.forIdx(2, array.size() - 1, ( idx, point ) -> {
+			T copy = createRandomPoint();
+			array.copy(point, copy);
+			found.add(copy);
+			indexes.add(idx);
+		});
+
+		assertEquals(23, indexes.size);
+		for (int i = 0; i < indexes.size; i++) {
+			assertEquals(i + 2, indexes.get(i));
+			checkEquals(array.getTemp(i + 2), found.get(i));
+		}
+	}
+
+	@Test public void set_index() {
+		PackedArray<T> alg = createAlg();
+
+		alg.append(createRandomPoint());
+		alg.append(createRandomPoint());
+		alg.append(createRandomPoint());
+
+		var p = createRandomPoint();
+		var copy = createRandomPoint();
+		alg.copy(p, copy);
+
+		alg.set(1, p);
+
+		// make sure set isn't writing to 'p'. yes this was a bug
+		checkEquals(p, copy);
+
+		// Retrieve the value and see if it's the same
+		T found = alg.getTemp(1);
+		checkEquals(p, found);
+	}
+
+//	@Test public void setTo_Standard() {checkSetTo(createAlg().getClass(), true);}
+
+	@Test public void isEquals() {
+		PackedArray<T> a = createAlg();
+		PackedArray<T> b = createAlg();
+
+		assertTrue(a.isEquals(b));
+
+		for (int i = 0; i < 4; i++) {
+			T p = createRandomPoint();
+			a.append(p);
+			assertFalse(a.isEquals(b));
+			b.append(p);
+			assertTrue(a.isEquals(b));
+		}
+	}
+}
